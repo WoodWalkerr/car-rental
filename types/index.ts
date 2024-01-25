17bb3c8cc5d1d1850d7da3679aea7cce3453e8c3
Melven <melvenbagolaque@gmail.com>
2024-01-25 12:45:08 +0800

    feature added components, svg and pages

---
+++ types/index.ts
@@ -0,0 +1,34 @@
+import { MouseEventHandler } from "react";
+
+export interface CustomButtonProps {
+    isDisabled?: boolean;
+    btnType?: "button" | "submit";
+    containerStyles?: string;
+    textStyles?: string;
+    title: string;
+    rightIcon?: string;
+    handleClick?: MouseEventHandler<HTMLButtonElement>;
+  }
+
+  export interface SearchManuFacturerProps {
+    manufacturer: string;
+    setManuFacturer: (manufacturer: string) => void;
+  }
+
+  export interface FilterProps {
+    manufacturer?: string;
+    year?: number;
+    model?: string;
+    limit?: number;
+    fuel?: string;
+  }
+
+  export interface OptionProps {
+    title: string;
+    value: string;
+  }
+
+  export interface CustomFilterProps {
+    title: string;
+    options: OptionProps[];
+  }
